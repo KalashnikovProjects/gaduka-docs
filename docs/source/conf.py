ffc649e5fc026c2bba0a948ae349436c3704e6f7
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2023-03-12 12:47:04 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = "furo"
-html_logo = "gaduka-icon.png"
+html_logo = "https://iili.io/HXlKaUP.md.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
